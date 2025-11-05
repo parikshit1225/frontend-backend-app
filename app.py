f7f1d3d4a68bb8c36507d0492c2adc5bd99970b5
Parikshit <157234139+DevExplorer25@users.noreply.github.com>
2025-11-05 12:52:37 +0530

    update

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def hello():
     return jsonify({"message": f"Hello, {name}! This response is from the backend (private subnet)."})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="127.0.0.1", port=5000)
